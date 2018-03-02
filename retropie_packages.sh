85d24ff00c616effbaa7eef6a45baf30afac8085
RetrOrangePi <retrorangepi@outlook.com>
2018-03-02 00:35:31 +0000

    adding some branding...

--- retropie_packages.sh
+++ retropie_packages.sh
@@ -49,7 +49,7 @@ if [[ "$(id -u)" -ne 0 ]]; then
     exit 1
 fi
 
-__backtitle="retropie.org.uk - RetroPie Setup. Installation folder: $rootdir for user $user"
+__backtitle="retrorangepi.org - RetrOrangePi - RetroPie Setup. Installation folder: $rootdir for user $user"
 
 source "$scriptdir/scriptmodules/system.sh"
 source "$scriptdir/scriptmodules/helpers.sh"
